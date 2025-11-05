cb323ebb74bd1c2b345aae2e3d35d5d705c70438
snowykte0426 <snowykte0426@naver.com>
2025-11-05 22:52:23 +0900

    refactor CreateConversationService and DeleteConversationService to use ConversationResponse and add transactional support

--- src/main/kotlin/team/exit_1/repo/backend/core/service/domain/service/CreateConversationService.kt
+++ src/main/kotlin/team/exit_1/repo/backend/core/service/domain/service/CreateConversationService.kt
@@ -1,7 +1,8 @@
 package team.exit_1.repo.backend.core.service.domain.service
 
 import org.springframework.stereotype.Service
-import team.exit_1.repo.backend.core.service.domain.data.dto.response.CreateConversationResponse
+import org.springframework.transaction.annotation.Transactional
+import team.exit_1.repo.backend.core.service.domain.data.dto.response.ConversationResponse
 import team.exit_1.repo.backend.core.service.domain.data.entity.Conversation
 import team.exit_1.repo.backend.core.service.domain.data.repository.ConversationJpaRepository
 import team.exit_1.repo.backend.core.service.global.config.MockDataConfig.Companion.MOCK_USER_ID
@@ -12,7 +13,8 @@ import java.util.*
 class CreateConversationService(
     private val conversationJpaRepository: ConversationJpaRepository
 ) {
-    fun execute(): CreateConversationResponse {
+    @Transactional
+    fun execute(): ConversationResponse {
         val conversationId = "conv_${UUID.randomUUID()}"
 
         val conversation = conversationJpaRepository.save(
@@ -22,9 +24,10 @@ class CreateConversationService(
                 this.timestamp = LocalDateTime.now()
             }
         )
-        return CreateConversationResponse(
+        return ConversationResponse(
             conversationId = conversation.id!!,
             userId = conversation.userId!!,
+            conversationStatus = conversation.status,
             timestamp = conversation.timestamp!!
         )
     }
--- src/main/kotlin/team/exit_1/repo/backend/core/service/domain/service/DeleteConversationService.kt
+++ src/main/kotlin/team/exit_1/repo/backend/core/service/domain/service/DeleteConversationService.kt
@@ -2,6 +2,7 @@ package team.exit_1.repo.backend.core.service.domain.service
 
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import team.exit_1.repo.backend.core.service.domain.data.repository.ConversationJpaRepository
 import team.exit_1.repo.backend.core.service.global.common.error.exception.ExpectedException
 
@@ -9,6 +10,7 @@ import team.exit_1.repo.backend.core.service.global.common.error.exception.Expec
 class DeleteConversationService(
     private val conversationJpaRepository: ConversationJpaRepository
 ) {
+    @Transactional
     fun execute(conversationId: String) {
         if (conversationJpaRepository.existsById(conversationId).not()) {
             throw ExpectedException(message = "대화가 존재하지 않습니다.", statusCode = HttpStatus.NOT_FOUND)
