d9c31a9737af49a199e7955eaa4dc91e9a089c73
snowykte0426 <snowykte0426@naver.com>
2025-11-05 18:48:43 +0900

    add BCryptPasswordEncoder bean for password encryption

--- src/main/kotlin/team/exit_1/backend/core/global/security/config/SecurityConfig.kt
+++ src/main/kotlin/team/exit_1/backend/core/global/security/config/SecurityConfig.kt
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configurers.FormLoginC
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
@@ -43,4 +44,8 @@ class SecurityConfig(
 
         return http.build()
     }
+
+
+    @Bean
+    fun passwordEncoder(): BCryptPasswordEncoder = BCryptPasswordEncoder()
 }
