b8fa3f7a717d661d48595ba0a700b0b51c98e91d
snowykte0426 <snowykte0426@naver.com>
2025-11-05 17:08:51 +0900

    initialize Spring Boot project with Gradle setup and basic configuration

---
+++ src/test/kotlin/team/exit_1/backend/core/CoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package team.exit_1.backend.core
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
